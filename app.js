60ece1d6e3b9e63ca8c92616ff29b60572bdb427
ricardovazquez03 <ricardo,vazquez1203@gmail.com>
2021-10-28 22:30:53 -0500

    Aplicación completa de la clase 6

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1. Puesta en común de los tipos de datos que vamos a necesitar
 // para guardar nuestras tareas
 
-// 2. Se arma un array 
+// 2. Se arma un array
 
 // 3. Se hace evidente que los datos tienen que estar afuera
 // Creamos un archivo JSON
@@ -23,40 +23,82 @@
 
 // 4. Se lleva el código a un modulo
 
-let archivoTareas = require('./tareas');
+let archivoTareas = require("./tareas");
 
 let accion = process.argv[2];
 
-switch(accion) {
-    case 'listar':
-        console.log('Listado de tareas');
-        let tareas = archivoTareas.leer();
-        // Sustituir el for por el método foreach
-        for (let i = 0;  i < tareas.length; i++) {
-            console.log(i + '. ' + tareas[i].titulo + ' - ' + tareas[i].estado);
-        }
-        console.log()
-        break;
+let tareas = [];
 
-    // Agregar caso de escribir json
+switch (accion) {
+  case "listar":
+    console.log();
+    console.log("Listado de tareas");
+    console.log("----------------");
 
-    // Crear tarea
-        // let titulo de tarea = process.argv[3];
-        // let objeto = {
-        //     titulo: process.argv[3],
-        //     estado: 'pendiente',
-        // }
-        // Mandamos a llamar a la funcion guardarTarea
+    tareas = archivoTareas.leerJSON();
+    // for (let i = 0;  i < tareas.length; i++) {
+    //     console.log(i + '. ' + tareas[i].titulo + ' -> ' + tareas[i].estado);
+    // }
 
-    // Micro desafío 1
-    // Atajar el caso en que no se envíe un parámetro
-    case undefined:
-        console.log('Tenés que pasarme una acción');
-        break;
+    // Micro desafío
+    // Armar la función de más arriba como array.forEach()
+    // Seguramente no implementen el index. Atajarlo.
 
-    // Micro desafío 2
-    default:
-        console.log('No entiendo qué me estás pidiendo');
-        console.log('Las acciones disponibles son: listar');
-        break;
+    tareas.forEach((tarea, index) => {
+      console.log(index + 1 + ". " + tarea.titulo + " -> " + tarea.estado);
+    });
+
+    console.log();
+    break;
+
+  // Micro desafío 2.C
+  // ---------------
+  // Escribir el caso de crear tarea
+  // Tener en cuenta que va a llegar el nombre de la tarea como segundo argumento
+  // Tener en cuenta que la tarea se crea en esto "pendiente"
+
+  case "crear":
+    console.log();
+    console.log("Nueva tarea creada");
+    console.log("------------------");
+
+    let titulo = process.argv[3];
+    let tarea = {
+      titulo: titulo,
+      estado: "pendiente",
+    };
+
+    archivoTareas.guardarTarea(tarea);
+
+    console.log(tarea.titulo + " -> " + tarea.estado);
+    console.log();
+    break;
+
+  // Micro desafío 3
+
+  case "filtrar":
+    let estado = process.argv[3];
+
+    console.log();
+    console.log("Tareas " + estado);
+    console.log("------------------");
+
+    let filtradas = archivoTareas.leerPorEstado(estado);
+
+    filtradas.forEach((tarea) => {
+      console.log(tarea.titulo);
+    });
+
+    break;
+
+  case undefined:
+    console.log("Tenés que pasarme una acción");
+    break;
+
+  default:
+    console.log("No entiendo qué me estás pidiendo");
+    console.log("Las acciones disponibles son: listar, crear, filtrar, ");
+    break;
 }
+
+console.log(archivoTareas);
--- tareas.js
+++ tareas.js
@@ -1,18 +1,42 @@
-const fs = require('fs');
-
-// Crear funcion escribirJSON([])
-
-// Crear funcion guardarTarea(tarea nueva) {
-    // Guardar esta tarea en el arreglo que tiene la función leer
-    // Actualizar el json (haciendo uso escribirJSON)
-// }
+const fs = require("fs");
 
 let archivoTareas = {
-    archivo: 'tareas.json',
-    leer: function () {
-        return JSON.parse(fs.readFileSync(this.archivo, 'utf-8'));
-    },
+  archivo: "./tareas.json",
 
-}
+  leerJSON: function () {
+    return JSON.parse(fs.readFileSync(this.archivo, "utf-8"));
+  },
+
+  // Micro desafío 2.A - Escritir la tarea en el archivo JSON
+  // ---------------
+  // 1. Escribir el método escribirJSON que recibirá un array de tareas
+  // 2. Utilizar el método JSON.stringify() para trasformar el array a formato JSON
+  // 3. Utilizar el método de fs.writeFileSync() para escribir el JSON en el archivo.
+  escribirJSON: function (tareas) {
+    fs.writeFileSync(this.archivo, JSON.stringify(tareas, null, " "));
+  },
+
+  // Micro desafío 2.B - Escribir la tarea en el archivo JSON
+  // ---------------
+  // 1. ...
+  // 2. ...
+  // 3. ...
+  guardarTarea: function (tarea) {
+    let tareas = this.leerJSON();
+    tareas.push(tarea);
+    this.escribirJSON(tareas);
+  },
+
+  // Micro desafío 3, aplicar filter para traer solo las tareas pendientes
+  leerPorEstado: (estado) => {
+    let tareas = this.leerJSON();
+
+    return tareas.filter((tarea) => {
+      return tarea.estado == estado;
+    });
+
+    // tareasFiltradas;
+  },
+};
 
 module.exports = archivoTareas;
